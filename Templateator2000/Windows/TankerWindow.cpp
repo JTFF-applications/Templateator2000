23b146dab951c7605dd7a4a772c6b9ec89a77c79
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2022-06-16 17:03:44 +0200

    Windows: Add GroupName to Tanker UI

--- Templateator2000/Windows/TankerWindow.cpp
+++ Templateator2000/Windows/TankerWindow.cpp
@@ -5,8 +5,11 @@
 #include "Utilities/Validators/QStringListValidator.h"
 #include "Windows/TankerWindow.h"
 
-TankerWindow::TankerWindow(QWidget* parent, const std::map<const std::string, const std::vector<Group>>& mission_data, std::function<void(Tanker)> on_ok, std::function<void()> on_cancel)
-	: QDialog(parent), m_missionData(mission_data), m_onOk(on_ok), m_onCancel(on_cancel)
+TankerWindow::TankerWindow(QWidget* parent,
+                           const std::map<const std::string, const std::vector<Group>>& mission_data,
+                           std::function<void(Tanker)> on_ok,
+                           std::function<void()> on_cancel)
+	: QDialog(parent), m_mission_data(mission_data), m_on_ok(on_ok), m_on_cancel(on_cancel)
 {
 	m_ui.setupUi(this);
 
@@ -14,18 +17,21 @@ TankerWindow::TankerWindow(QWidget* parent, const std::map<const std::string, co
 
 	m_ui.departure->setCompleter(new QCompleter(Moose::GetQtAirbases(), this));
 	m_ui.parking_size->setCompleter(new QCompleter(Moose::GetQtParkings(), this));
-	m_ui.pattern->setCompleter(new QCompleter(Mission::DataToUnitName(m_missionData), this));
-	m_ui.escort->setCompleter(new QCompleter(Mission::DataToGroupName(m_missionData), this));
+	m_ui.pattern->setCompleter(new QCompleter(Mission::DataToUnitName(m_mission_data), this));
+	m_ui.group->setCompleter(new QCompleter(Mission::DataToGroupName(m_mission_data), this));
+	m_ui.escort->setCompleter(new QCompleter(Mission::DataToGroupName(m_mission_data), this));
 
-	m_ui.frequency->setValidator(new QRegularExpressionValidator(QRegularExpression("^[1-3][0-9]{2}[.][0-9](00|25|50|75)$")));
+	m_ui.frequency->setValidator(
+		new QRegularExpressionValidator(QRegularExpression("^[1-3][0-9]{2}[.][0-9](00|25|50|75)$")));
 	m_ui.tacan_morse->setValidator(new QRegularExpressionValidator(QRegularExpression("^[A-Z]{3}$")));
 	m_ui.departure->setValidator(new QStringListValidator(Moose::GetQtAirbases()));
 	m_ui.parking_size->setValidator(new QStringListValidator(Moose::GetQtParkings()));
-	m_ui.pattern->setValidator(new QStringListValidator(Mission::DataToUnitName(m_missionData)));
-	m_ui.escort->setValidator(new QStringListValidator(Mission::DataToGroupName(m_missionData)));
+	m_ui.pattern->setValidator(new QStringListValidator(Mission::DataToUnitName(m_mission_data)));
+	m_ui.group->setValidator(new QStringListValidator(Mission::DataToGroupName(m_mission_data)));
+	m_ui.escort->setValidator(new QStringListValidator(Mission::DataToGroupName(m_mission_data)));
 
-	connect(m_ui.ok_btn, &QPushButton::clicked, this, &TankerWindow::on_ok_clicked);
-	connect(m_ui.cancel_btn, &QPushButton::clicked, this, &TankerWindow::on_cancel_clicked);
+	connect(m_ui.ok_btn, &QPushButton::clicked, this, &TankerWindow::onOkClicked);
+	connect(m_ui.cancel_btn, &QPushButton::clicked, this, &TankerWindow::onCancelClicked);
 }
 
 TankerWindow::~TankerWindow()
@@ -33,18 +39,24 @@ TankerWindow::~TankerWindow()
 	delete m_ui.departure->completer();
 	delete m_ui.parking_size->completer();
 	delete m_ui.pattern->completer();
+	delete m_ui.group->completer();
 	delete m_ui.escort->completer();
 
 	delete m_ui.frequency->validator();
-	delete m_ui.tacan_morse->validator();
 	delete m_ui.departure->validator();
+	delete m_ui.tacan_morse->validator();
 	delete m_ui.parking_size->validator();
+	delete m_ui.pattern->validator();
+	delete m_ui.group->validator();
+	delete m_ui.escort->validator();
 }
 
 void TankerWindow::SetTanker(const Tanker& tk) const
 {
 	m_ui.type->setCurrentIndex(m_ui.type->findText(tk.Type == Tanker::Type::Fixed ? "Fixed" : "On Demand"));
-	m_ui.coalition->setCurrentIndex(m_ui.coalition->findText(tk.Coalition == Coalition::Blue ? "Blue" : tk.Coalition == Coalition::Red ? "Red" : "Neutral"));
+	m_ui.coalition->setCurrentIndex(m_ui.coalition->findText(
+		tk.Coalition == Coalition::Blue ? "Blue" : tk.Coalition == Coalition::Red ? "Red" : "Neutral"));
+	m_ui.group->setText(tk.GroupName.c_str());
 	m_ui.pattern->setText(tk.PatternUnit.c_str());
 	m_ui.departure->setText(Moose::GetNameFromMooseAirbase(tk.DepartureBase).c_str());
 	m_ui.parking_size->setText(Moose::GetNameFromMooseParking(tk.ParkingSize).c_str());
@@ -64,16 +76,21 @@ void TankerWindow::SetTanker(const Tanker& tk) const
 	m_ui.callsign_nb->setValue(tk.CallsignNb);
 }
 
-void TankerWindow::on_ok_clicked()
+void TankerWindow::onOkClicked()
 {
 	try
 	{
 		Tanker tanker = {
 			.Type = m_ui.type->currentText() == "Fixed" ? Tanker::Type::Fixed : Tanker::Type::OnDemand,
-			.Coalition = m_ui.coalition->currentText() == "Blue" ? Coalition::Blue : m_ui.coalition->currentText() == "Red" ? Coalition::Red : Coalition::Neutral,
+			.Coalition = m_ui.coalition->currentText() == "Blue"
+				             ? Coalition::Blue
+				             : m_ui.coalition->currentText() == "Red"
+					               ? Coalition::Red
+					               : Coalition::Neutral,
 			.PatternUnit = m_ui.pattern->text().toStdString(),
 			.DepartureBase = Moose::GetMooseAirbaseFromName(m_ui.departure->text().toStdString()),
 			.ParkingSize = Moose::GetMooseParkingFromName(m_ui.parking_size->text().toStdString()),
+			.GroupName = m_ui.group->text().toStdString(),
 			.EscortGroup = m_ui.escort->text().toStdString(),
 			.Callsign = Moose::GetMooseCallsignFromName(m_ui.callsign->currentText().toStdString()),
 			.Frequency = m_ui.frequency->text().toStdString(),
@@ -90,9 +107,10 @@ void TankerWindow::on_ok_clicked()
 			.CallsignNb = m_ui.callsign_nb->value()
 		};
 
-		if (!Mission::DataToUnitName(m_missionData).contains(tanker.PatternUnit.c_str()))
+		if (!Mission::DataToUnitName(m_mission_data).contains(tanker.PatternUnit.c_str()))
 			throw std::exception("Invalid pattern unit !");
-		if (!tanker.EscortGroup.empty() || Mission::DataToGroupName(m_missionData).contains(tanker.EscortGroup.c_str()))
+		if (!tanker.EscortGroup.empty() || Mission::DataToGroupName(m_mission_data).
+		    contains(tanker.EscortGroup.c_str()))
 			throw std::exception("Invalid escort group !");
 		if (tanker.Frequency.empty() || tanker.Frequency.size() != 7)
 			throw std::exception("Invalid radio frequency !");
@@ -100,16 +118,15 @@ void TankerWindow::on_ok_clicked()
 			throw std::exception("Invalid tacan morse code !");
 
 		close();
-		std::invoke(m_onOk, tanker);
-	}
-	catch (const std::exception& except)
+		std::invoke(m_on_ok, tanker);
+	} catch (const std::exception& except)
 	{
 		m_ui.error_msg->setText(except.what());
 	}
 }
 
-void TankerWindow::on_cancel_clicked()
+void TankerWindow::onCancelClicked()
 {
 	close();
-	std::invoke(m_onCancel);
+	std::invoke(m_on_cancel);
 }
--- Templateator2000/Windows/TankerWindow.h
+++ Templateator2000/Windows/TankerWindow.h
@@ -2,27 +2,35 @@
 
 #include <vector>
 
-#include "ui_TankerWindow.h"
 #include "Models/Tanker.h"
 #include "Utilities/DCS/Group.h"
+#include "ui_TankerWindow.h"
 
 class TankerWindow final : public QDialog
 {
 	Q_OBJECT
 
 public:
-	TankerWindow(QWidget* parent, const std::map<const std::string, const std::vector<Group>>& mission_data, std::function<void(Tanker)> on_ok, std::function<void(void)> on_cancel);
-	~TankerWindow() override;
+	TankerWindow(QWidget* parent,
+	             const std::map<const std::string, const std::vector<Group>>& mission_data,
+	             std::function<void(Tanker)> on_ok,
+	             std::function<void(void)> on_cancel);
+	virtual ~TankerWindow() override;
+
+	TankerWindow(const TankerWindow&) = delete;
+	TankerWindow(TankerWindow&&) = delete;
+	TankerWindow& operator=(const TankerWindow&) = delete;
+	TankerWindow& operator=(TankerWindow&&) = delete;
 
 	void SetTanker(const Tanker& tk) const;
 
 private slots:
-	void on_ok_clicked();
-	void on_cancel_clicked();
+	void onOkClicked();
+	void onCancelClicked();
 
 private:
-	const std::map<const std::string, const std::vector<Group>> m_missionData;
-	std::function<void(Tanker)> m_onOk;
-	std::function<void()> m_onCancel;
+	const std::map<const std::string, const std::vector<Group>> m_mission_data;
+	std::function<void(Tanker)> m_on_ok;
+	std::function<void()> m_on_cancel;
 	Ui::TankerWindow m_ui = {};
 };

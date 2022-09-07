0d3874d4205fe44d541cf24f3a2b77e796e4f287
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2022-09-07 11:58:41 +0200

    Lua Lib: Add formatting to SaveTable

--- Templateator2000/templates/templateator_lua_lib.lua
+++ Templateator2000/templates/templateator_lua_lib.lua
@@ -1021,29 +1021,81 @@ JSON = OBJDEF:new()
 --               ----      TABLE SAVE -----                 --
 -- ******************************************************** --
 
-function SaveTable(Table)
-   local savedTables = {} -- used to record tables that have been saved, so that we do not go into an infinite recursion
-   local outFuncs = {
-      ['string']  = function(value) return string.format("%q",value) end;
-      ['boolean'] = function(value) if (value) then return 'true' else return 'false' end end;
-      ['number']  = function(value) if(math.tointeger(value)) then return math.tointeger(value) else return string.format('%f',value) end end;
-   }
-   local outFuncsMeta = {
-      __index = function(t,k) error('Invalid Type For SaveTable: '..k) end      
-   }
-   setmetatable(outFuncs,outFuncsMeta)
-   local tableOut = function(value)
-      if (savedTables[value]) then
-         error('There is a cyclical reference (table value referencing another table value) in this set.');
-      end
-      local outValue = function(value) return outFuncs[type(value)](value) end
-      local out = '{'
-      for i,v in pairs(value) do out = out..'['..outValue(i)..']='..outValue(v)..';' end
-      savedTables[value] = true; --record that it has already been saved
-      return out..'}'
-   end
-   outFuncs['table'] = tableOut;
-   return tableOut(Table);
+function SaveTable(node)
+    local cache, stack, output = {},{},{}
+    local depth = 1
+    local output_str = "{\n"
+
+    while true do
+        local size = 0
+        for k,v in pairs(node) do
+            size = size + 1
+        end
+
+        local cur_index = 1
+        for k,v in pairs(node) do
+            if (cache[node] == nil) or (cur_index >= cache[node]) then
+
+                if (string.find(output_str,"}",output_str:len())) then
+                    output_str = output_str .. ",\n"
+                elseif not (string.find(output_str,"\n",output_str:len())) then
+                    output_str = output_str .. "\n"
+                end
+
+                -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+                table.insert(output,output_str)
+                output_str = ""
+
+                local key
+                if (type(k) == "number" or type(k) == "boolean") then
+                    key = "["..tostring(k).."]"
+                else
+                    key = "['"..tostring(k).."']"
+                end
+
+                if (type(v) == "number" or type(v) == "boolean") then
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = "..tostring(v)
+                elseif (type(v) == "table") then
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = {\n"
+                    table.insert(stack,node)
+                    table.insert(stack,v)
+                    cache[node] = cur_index+1
+                    break
+                else
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = '"..tostring(v).."'"
+                end
+
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                else
+                    output_str = output_str .. ","
+                end
+            else
+                -- close the table
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                end
+            end
+
+            cur_index = cur_index + 1
+        end
+
+        if (size == 0) then
+            output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+        end
+
+        if (#stack > 0) then
+            node = stack[#stack]
+            stack[#stack] = nil
+            depth = cache[node] == nil and depth + 1 or depth - 1
+        else
+            break
+        end
+    end
+
+    -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+    table.insert(output,output_str)
+    return table.concat(output)
 end
 
 -- ******************************************************** --

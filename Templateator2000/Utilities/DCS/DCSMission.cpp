95d0f3d9496d2b665e8e5d5bf16690110c71033e
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2022-09-07 11:59:02 +0200

    DCS: Add support for KC10 in tankers

--- Templateator2000/Utilities/DCS/DCSMission.cpp
+++ Templateator2000/Utilities/DCS/DCSMission.cpp
@@ -86,7 +86,7 @@ const std::vector<std::string> DcsMission::Tankers(const Coalition::Side& coalit
 			if (unit.Coalition() == coalition)
 			{
 				const std::string& type = unit.Type();
-				if (type == "KC130" || type == "KC-135" || type == "KC135MPRS" || type == "S-3B Tanker")
+				if (type == "KC130" || type == "KC-135" || type == "KC135MPRS" || type == "S-3B Tanker" ||type == "KC_10_Extender" || type == "KC_10_Extender_D")
 					res.push_back(unit.Name());
 			}
 	return res;
